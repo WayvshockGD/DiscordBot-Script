91b5665af20f521814730346195f60c75fe9e15e
WayvshockGD <pizza5803@gmail.com>
2021-10-20 20:42:27 +0300

    Added jsdoc comments

--- lib/types/CommandTypes.ts
+++ lib/types/CommandTypes.ts
@@ -6,11 +6,20 @@ export type EmbedCommandDataMap = Map<string, EmbedCommandData>;
 export type MapWithID<Val> = Map<string, Val>;
 
 export interface CommandMetaData<V> {
+    /**
+     * The name of the whole command.
+     */
     name: string;
+    /**
+     * Basically just everything that makes the thing go.
+     */
     code: CommandObjectData<V>;
 }
 
 export interface CommandObjectData<V = boolean> {
+    /**
+     * Set the embed data here and it will automatically set it in the loader.
+     */
     embeds?: EmbedCommandData,
     /**
      * The main part of this. It is used to execute scripts...
@@ -20,10 +29,19 @@ export interface CommandObjectData<V = boolean> {
      * For use with setting message ids and such...
      */
     executeMessage?: (ctx: CommandContextData) => void;
+    /**
+     * Args checker for checking arguments...
+     */
     args?: (args: string[]) => V;
 }
 
 export interface CommandContextData {
+    /**
+     * Embed map for setting or getting embeds by id.
+     */
     embeds: EmbedCommandDataMap;
+    /**
+     * Message content for things if you need.
+     */
     message: Eris.Message;
 }
